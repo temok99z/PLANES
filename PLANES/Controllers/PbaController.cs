2384ac9a606e43294315a53dc105404c8e91d694
dummy99z <47759515+dummy99z@users.noreply.github.com>
2020-04-18 19:39:01 -0500

    Agregar archivos de proyecto.

---
+++ PLANES/Controllers/PbaController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PLANES.Controllers
+{
+    public class PbaController : Controller
+    {
+        // 
+        // GET: /Pba/
+
+        public string Index()
+        {
+            return ">>>>>>>>>>>>>>>>>>>>>>>>>>>This is my default action..FROM Pba.";
+            return ">>>>>>>>>>>>>>>>>>>>>>>>>>>This is my default action..FROM Pba.";
+        }
+
+        // 
+        // GET: /Pba/Welcome/ 
+
+        public string Welcome( )
+        {
+            return "**********************************This is the Welcome action method..FROM Pba/Welcome/.";
+            return "**********************************This is the Welcome action method..FROM Pba/Welcome/.";
+        }
+    }
+}
